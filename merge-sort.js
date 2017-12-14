18f3e7132542f34840caa49a66e91ec8ef54604c
Deepak Ranjan Jena <32715767+deepak-ranjan-jena@users.noreply.github.com>
2017-12-14 15:28:50 -0500

    Create merge-sort.js

---
+++ merge-sort.js
@@ -0,0 +1,39 @@
+/*
+* Javascript array sorting using merge sort
+*/
+let inputArray = [9, 4, 5, 7, 2, 4, 1, 0, -2, -1];
+((input) => {
+  
+  var mergeArray = (leftArray, rightArray) => {
+      let result  = [],
+          i      = 0,
+          j      = 0;
+          
+      while (i < leftArray.length && j < rightArray.length){
+          if (leftArray[i] < rightArray[j]){
+              result.push(leftArray[i++]);
+          } else {
+              result.push(rightArray[j++]);
+          }
+      }
+
+      return result.concat(leftArray.slice(i)).concat(rightArray.slice(j));
+  }  
+  
+  var mergeSort = (arr) => {
+
+    // return the array incase length less than 2
+    if (arr.length < 2) {
+          return arr;
+    }
+
+    var pivot = Math.floor(arr.length / 2);
+
+    return mergeArray(mergeSort(items.slice(0, middle)), mergeSort(items.slice(middle)));
+  }
+  
+  // call the mergesort to sort the input array
+  const result = mergeSort(input);
+  console.log(result);
+  
+})(inputArray);
